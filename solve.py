af5d243d2999934e4cbde1afddd1a030c339f058
Michael Mooney <82550035+embium@users.noreply.github.com>
2022-02-08 16:49:31 -0600

    Refactoring

--- solve.py
+++ solve.py
@@ -1,149 +1,126 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import json
+import asyncio
 import os
-import requests
 import shutil
 import sys
-import time
 import tempfile
-import wave
+import time
+import utils
 
-from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
-from playwright_stealth import stealth_sync
-from pydub import AudioSegment
+from playwright.async_api import async_playwright, TimeoutError
+from playwright_stealth import stealth_async
 from urllib.parse import urlparse
-from vosk import Model, KaldiRecognizer, SetLogLevel
 
-from flask import Flask, request
 
-app = Flask(__name__)
+class Solver(object):
 
-SetLogLevel(-1)
+    def __init__(self, pageurl, sitekey, proxy=None, headless=False):
+        self.pageurl = pageurl
+        self.sitekey = sitekey
+        self.proxy = proxy
+        self.headless = headless
+    
+    async def start(self):
+        self.browser = await self.get_browser(self.headless)
+        self.page = await self.new_page()
+        await self.apply_stealth()
+        await self.reroute_requests()
+        await self.goto_page()
+        await self.click_checkbox()
+        await self.click_audio_button()
+        result = None
+        while 1:
+            if not await self.check_detection(timeout=5):
+                await self.solve_audio()
+                result = await self.get_recaptcha_response()
+                if result:
+                    break
+            else:
+                break
+        await self.cleanup()
+        if result:
+            return result
 
-@app.route("/", methods=['GET'])
-def solve():
-    args = request.args
-    if 'pageurl' not in args or 'sitekey' not in args:
-        return 'Parameters pageurl and sitekey are required'
-    pageurl = args['pageurl']
-    sitekey = args['sitekey']
-    response = main(pageurl, sitekey, headless=True)
-    return response
+    async def get_browser(self, headless):
+        playwright = await async_playwright().start()
+        if self.proxy:
+            self.proxy = {'server': self.proxy}
+        browser = await playwright.webkit.launch(headless=headless, proxy=self.proxy)
+        return browser
+    
+    async def new_page(self):
+        page = await self.browser.new_page()
+        return page
 
-def mp3_to_wav(mp3_filename):
-    wav_filename = mp3_filename.replace(".mp3", ".wav")
-    segment = AudioSegment.from_mp3(mp3_filename)
-    sound = segment.set_channels(1).set_frame_rate(16000)
-    sound.export(wav_filename, format="wav")
-    return wav_filename
+    async def apply_stealth(self):
+        await stealth_async(self.page)
 
-def get_text(mp3_filename):
-    wav_filename = mp3_to_wav(mp3_filename)
-    wf = wave.open(wav_filename, "rb")
-    model = Model("model")
-    rec = KaldiRecognizer(model, wf.getframerate())
-    while True:
-        data = wf.readframes(4000)
-        if len(data) == 0:
-            break
-        rec.AcceptWaveform(data)
-    return json.loads(rec.FinalResult())
-
-def save_file(file, data, binary=False):
-    mode = "w" if not binary else "wb"
-    with open(file, mode) as file:
-        file.write(data)
-
-def get_page(
-        url,
-        binary=False,
-        timeout=300):
-    with requests.Session() as session:
-        response = session.get(
-            url,
-            timeout=timeout)
-        if binary:
-            return response.content
-        return response.text
-
-def main(pageurl, sitekey, headless=False):
-    # Code we will inject into the page
-    widget_code = (f"<script src=https://www.google.com/recaptcha/api.js?hl=en async defer></script>"
-                   f"<div class=g-recaptcha data-sitekey={sitekey}>"
-                    "</div>")
-    parsed_url = urlparse(pageurl)
-    scheme = parsed_url.scheme
-    netloc = parsed_url.netloc
-    with sync_playwright() as p:
-        # Launch web browser
-        browser = p.webkit.launch(headless=headless)
-        # Open a new page
-        page = browser.new_page()
-        # Apply anti-bot stealth
-        stealth_sync(page)
-        # Hacky - Reroute request from supplied url to a page with only the recaptcha box
-        # A better solution may be available but I haven't found one
-        page.route(f"{scheme}://{netloc}/*", lambda route: route.fulfill(
+    async def reroute_requests(self):
+        parsed_url = urlparse(pageurl)
+        scheme = parsed_url.scheme
+        netloc = parsed_url.netloc
+        await self.page.route(f"{scheme}://{netloc}/*", lambda route: route.fulfill(
             content_type="text/html",
-            body=f"{widget_code}")
+            body="<script src=https://code.jquery.com/jquery-3.6.0.min.js></script>"
+                 f"<script src=https://www.google.com/recaptcha/api.js?hl=en></script>"
+                 f"<div class=g-recaptcha data-sitekey={sitekey}>"
+                "</div>")
         )
-        # Go to url
-        page.goto(pageurl, wait_until='commit')
-        # Wait for load state
-        page.wait_for_load_state("load")
-        # Wait for checkbox frame to be available
-        page.wait_for_selector("iframe[src*=\"api2/anchor\"]", state="visible")
-        checkbox_frame = next(frame for frame in page.frames if "api2/anchor" in frame.url)
-        # Wait for checkbox to be visible
-        checkbox = checkbox_frame.wait_for_selector("#recaptcha-anchor")
-        # Click checkbox
-        checkbox.click()
-        # Wait for image frame to be visible
-        page.wait_for_selector("iframe[src*=\"api2/bframe\"]", state="visible")
-        image_frame = next(frame for frame in page.frames if "api2/bframe" in frame.url)
-        # Obtain audio button
-        audio_button = image_frame.wait_for_selector('#recaptcha-audio-button')
-        # Click audio button
-        audio_button.click()
-        # Wait for play button
-        play_button = image_frame.wait_for_selector("#audio-source", state="attached")
-        # Extract audio file's location
-        audio_source = play_button.evaluate("node => node.src")
-        # Download audio file
-        audio_data = get_page(audio_source, binary=True)
-        # Create temporary directory for audio file
+
+    async def goto_page(self):
+        await self.page.goto(self.pageurl, wait_until='commit')
+        await self.page.wait_for_load_state("load")
+
+    async def click_checkbox(self):
+        checkbox_frame = next(frame for frame in self.page.frames if "api2/anchor" in frame.url)
+        checkbox = await checkbox_frame.wait_for_selector("#recaptcha-anchor")
+        await checkbox.click()
+
+    async def click_audio_button(self):
+        await self.page.wait_for_selector("iframe[src*=\"api2/bframe\"]", state="visible")
+        self.image_frame = next(frame for frame in self.page.frames if "api2/bframe" in frame.url)
+        audio_button = await self.image_frame.wait_for_selector('#recaptcha-audio-button')
+        await audio_button.click()
+
+    async def check_detection(self, timeout):
+        timeout = time.time() + timeout
+        while time.time() < timeout:
+            content = await self.image_frame.content()
+            if 'Try again later' in content:
+                return 'detected'
+            elif 'please solve more' in content:
+                return 'solve more'
+    
+    async def solve_audio(self):
+        play_button = await self.image_frame.wait_for_selector("#audio-source", state="attached")
+        audio_source = await play_button.evaluate("node => node.src")
+        audio_data = await utils.get_page(audio_source, binary=True)
         tmpd = tempfile.mkdtemp()
-        # Save audio file
         tmpf = os.path.join(tmpd, "audio.mp3")
-        save_file(tmpf, data=audio_data, binary=True)
-        # Speech to text recognition using Vosk
-        audio_response =get_text(tmpf)
-        # Obtain audio response input field
-        audio_response_input = image_frame.wait_for_selector("#audio-response", state="attached")
-        # Fill in audio responsed input field
-        audio_response_input.fill(audio_response['text'])
-        # Obtain recaptcha verify button
-        recaptcha_verify_button = image_frame.wait_for_selector("#recaptcha-verify-button", state="attached")
-        # Click recaptcha verify button
-        recaptcha_verify_button.click()
-        # Wait until recaptcha response isn't empty
-        page.wait_for_function('document.getElementById("g-recaptcha-response").value !== ""')
-        # Extract recaptcha response
-        recaptcha_response = page.evaluate('document.getElementById("g-recaptcha-response").value')
-        # Close browser
-        browser.close()
-        # Remove temporary directory
+        await utils.save_file(tmpf, data=audio_data, binary=True)
+        audio_response = await utils.get_text(tmpf)
+        audio_response_input = await self.image_frame.wait_for_selector("#audio-response", state="attached")
+        await audio_response_input.fill(audio_response['text'])
+        recaptcha_verify_button = await self.image_frame.wait_for_selector("#recaptcha-verify-button", state="attached")
+        await recaptcha_verify_button.click()
         shutil.rmtree(tmpd)
-        # Return recaptcha response
+
+    async def get_recaptcha_response(self):
+        await self.page.wait_for_function('document.getElementById("g-recaptcha-response").value !== ""')
+        recaptcha_response = await self.page.evaluate('document.getElementById("g-recaptcha-response").value')
         return recaptcha_response
 
+    async def cleanup(self):
+        await self.browser.close()
+
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        app.run()
+    pageurl = sys.argv[1]
+    sitekey = sys.argv[2]
+    if len(sys.argv) == 4:
+        proxy = sys.argv[3]
     else:
-        pageurl = sys.argv[1]
-        sitekey = sys.argv[2]
-        response = main(pageurl, sitekey)
-        print(response)
+        proxy = None
+    solver = Solver(pageurl, sitekey, proxy=proxy)
+    print(asyncio.run(solver.start()))
---
+++ utils.py
@@ -0,0 +1,119 @@
+import aiohttp
+import aiofiles
+import asyncio
+import json
+import requests
+import sys
+import wave
+
+from functools import partial, wraps
+from pydub import AudioSegment
+from vosk import Model, KaldiRecognizer, SetLogLevel
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+SetLogLevel(-1)
+
+def threaded(func):
+    @wraps(func)
+    async def wrap(*args, **kwargs):
+        loop = asyncio.get_event_loop()
+        return await loop.run_in_executor(None, partial(func, *args, **kwargs))
+    return wrap
+
+def mp3_to_wav(mp3_filename):
+    wav_filename = mp3_filename.replace(".mp3", ".wav")
+    segment = AudioSegment.from_mp3(mp3_filename)
+    sound = segment.set_channels(1).set_frame_rate(16000)
+    sound.export(wav_filename, format="wav")
+    return wav_filename
+
+@threaded
+def get_text(mp3_filename):
+    wav_filename = mp3_to_wav(mp3_filename)
+    wf = wave.open(wav_filename, "rb")
+    model = Model("model")
+    rec = KaldiRecognizer(model, wf.getframerate())
+    while True:
+        data = wf.readframes(4000)
+        if len(data) == 0:
+            break
+        rec.AcceptWaveform(data)
+    return json.loads(rec.FinalResult())
+
+async def save_file(file, data, binary=False):
+    mode = "w" if not binary else "wb"
+    async with aiofiles.open(file, mode=mode) as f:
+        await f.write(data)
+
+
+async def load_file(file, binary=False):
+    mode = "r" if not binary else "rb"
+    async with aiofiles.open(file, mode=mode) as f:
+        return await f.read()
+
+def merge_dict(dict1, dict2):
+    """Merge two dictionaries into new one."""
+    new_dict = {}
+    if dict1:
+        new_dict.update(dict1)
+    if dict2:
+        new_dict.update(dict2)
+    return new_dict
+
+@threaded
+def get_page_win(
+        url,
+        proxy=None,
+        proxy_auth=None,
+        binary=False,
+        verify=False,
+        timeout=300):
+    proxies = None
+    if proxy:
+        if proxy_auth:
+            proxy = proxy.replace("http://", "")
+            username = proxy_auth['username']
+            password = proxy_auth['password']
+            proxies = {
+                "http": f"http://{username}:{password}@{proxy}",
+                "https": f"http://{username}:{password}@{proxy}"}
+        else:
+            proxies = {"http": proxy, "https": proxy}
+    with requests.Session() as session:
+        response = session.get(
+            url,
+            proxies=proxies,
+            verify=verify,
+            timeout=timeout)
+        if binary:
+            return response.content
+        return response.text
+
+
+async def get_page(
+        url,
+        proxy=None,
+        proxy_auth=None,
+        binary=False,
+        verify=False,
+        timeout=300):
+    if sys.platform == "win32":
+        # SSL Doesn't work on aiohttp through ProactorLoop so we use Requests
+        return await get_page_win(
+            url, proxy, proxy_auth, binary, verify, timeout)
+    else:
+        if proxy_auth:
+            proxy_auth = aiohttp.BasicAuth(
+                proxy_auth['username'], proxy_auth['password'])
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                    url,
+                    proxy=proxy,
+                    proxy_auth=proxy_auth,
+                    verify_ssl=verify,
+                    timeout=timeout) as response:
+                if binary:
+                    return await response.read()
+                return await response.text()
